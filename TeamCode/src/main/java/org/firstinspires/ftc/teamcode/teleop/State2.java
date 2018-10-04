b3bb836d3f403badf32466a8cc3b7cc3b3f6b804
Robert Woodley <blwoodley@yahoo.com>
2018-10-04 10:14:01 -0500

    example how to handle touch sensor.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/State2.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/State2.java
@@ -1,13 +1,17 @@
 package org.firstinspires.ftc.teamcode.teleop;
 
+import com.qualcomm.robotcore.hardware.TouchSensor;
+
 import org.firstinspires.ftc.teamcode.stateMachine.StateBase;
 import org.firstinspires.ftc.teamcode.stateMachine.StateMachine;
 
 import static org.firstinspires.ftc.teamcode.stateMachine.LoggerWrapper.log;
 
 public class State2 extends StateBase {
+    final TouchSensor touchSensor;
     State2(StateMachine stateMachine) {
         super(stateMachine);
+        touchSensor = hardwareMap.touchSensor.get("touch_sensor");
     }
 
     public void dpad_up_changed(boolean dpad_up) {
@@ -21,5 +25,7 @@ public class State2 extends StateBase {
         if (dpad_down)
             stateMachine.updateState(1);
     }
-
+    public void postEventsCallback() {
+        addTelemetry("touch sensor", "%f", touchSensor.getValue());
+    }
 }
