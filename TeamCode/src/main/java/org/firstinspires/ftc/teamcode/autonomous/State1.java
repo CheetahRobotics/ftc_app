7c0a43dc9755c5a08f4feed7f76fea2f7e038f69
omarcoding <omarionjmfranklin@gmail.com>
2018-10-13 12:09:22 -0500

    start working on generic DrivingState.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/State1.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/State1.java
@@ -7,29 +7,12 @@ import com.qualcomm.robotcore.hardware.TouchSensor;
 import org.firstinspires.ftc.teamcode.stateMachine.StateBase;
 import org.firstinspires.ftc.teamcode.stateMachine.StateMachine;
 
-public class State1 extends StateBase {
-    private final DcMotor leftDrive;
-    private final DcMotor rightDrive;
+public class State1 extends DrivingState {
     State1(StateMachine stateMachine) {
-        super(stateMachine);
-        leftDrive = hardwareMap.get(DcMotor.class, "motor_2");
-        rightDrive = hardwareMap.get(DcMotor.class, "motor_1");
-    }
-
-    @Override
-    public void timeUpdate(double sinceOpModePlay, double sinceStateStart) {
-        if (sinceStateStart > 2.0) {
-            stateMachine.updateState(2);
-        }
-        addTelemetry("Time", "%f %f", sinceOpModePlay, sinceStateStart);
-    }
-
-    @Override
-    public void postEventsCallback() {
-        leftDrive.setDirection(DcMotor.Direction.FORWARD);
-        leftDrive.setPower(0.5);
-        rightDrive.setDirection(DcMotor.Direction.FORWARD);
-        rightDrive.setPower(-0.5);
-        addTelemetry("Motor", "Driving forward");
+        super(stateMachine,
+                2.,
+                2,
+                0.5,
+                -.5);
     }
 }
