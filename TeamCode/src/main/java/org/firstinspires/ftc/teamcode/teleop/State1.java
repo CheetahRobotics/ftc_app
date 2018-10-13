8d3b7646d07a21bf4860dcd3d17515124dd3ca6d
nathanonealjr <nathanonealjr@gmail.com>
2018-10-13 12:14:02 -0500

    Got teleop running

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/State1.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/State1.java
@@ -2,50 +2,23 @@ package org.firstinspires.ftc.teamcode.teleop;
 
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.TouchSensor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.util.Range;
 
 import org.firstinspires.ftc.teamcode.stateMachine.StateBase;
 import org.firstinspires.ftc.teamcode.stateMachine.StateMachine;
 
 public class State1 extends StateBase {
-    private final DcMotor leftDrive;
+    private ElapsedTime runtime = new ElapsedTime();
+    private DcMotor leftDrive ;
+    private DcMotor rightDrive ;
+
     State1(StateMachine stateMachine) {
         super(stateMachine);
         leftDrive  = hardwareMap.get(DcMotor.class, "motor_1");
+        rightDrive = hardwareMap.get(DcMotor.class, "motor_2");
     }
 
-    @Override
-    public void dpadUpChanged(boolean dpad_up) {
-        if (dpad_up) {
-            stateMachine.updateState(2);
-            leftDrive.setPower(0.0);
-        }
-    }
-
-    @Override
-    public void dpadDownChanged(boolean dpad_down) {
-        if (dpad_down) {
-            stateMachine.updateState(3);
-            leftDrive.setPower(0.0);
-        }
-    }
-
-    @Override
-    public void dpadLeftChanged(boolean dpad_left) {
-        if (dpad_left) {
-            leftDrive.setDirection(DcMotor.Direction.FORWARD);
-            leftDrive.setPower(0.5);
-            addTelemetry("Motor", "Driving forward");
-        }
-    }
-
-    @Override
-    public void dpadRightChanged(boolean dpad_right) {
-        if (dpad_right) {
-            leftDrive.setDirection(DcMotor.Direction.REVERSE);
-            leftDrive.setPower(0.5);
-            addTelemetry("Motor", "Driving backwards");
-        }
-    }
 
     @Override
     public void timeUpdate(double sinceOpModePlay, double sinceStateStart) {
@@ -59,6 +32,16 @@ public class State1 extends StateBase {
 
     @Override
     public void postEventsCallback() {
+
+        double leftPower;
+        double rightPower;
+
+        double drive = -gamepad.left_stick_x;
+        double turn  = gamepad.right_stick_y;
+        leftPower    = Range.clip(drive + turn, -1.0, 1.0) ;
+        rightPower   = Range.clip(drive - turn, -1.0, 1.0) ;
+        leftDrive.setPower(leftPower);
+        rightDrive.setPower(rightPower);
 
         // this is called after all events
         addTelemetry("LeftStick", "X: %f, Y: %f",
