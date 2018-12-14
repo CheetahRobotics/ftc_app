992ce96f35e1cb97b0e9f7b61b8aedc09610175e
Robert Woodley <blwoodley@yahoo.com>
2018-12-13 22:27:53 -0600

    Add turning code based on X coordinate.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/cameraAutonomous/State1.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/cameraAutonomous/State1.java
@@ -3,7 +3,10 @@ package org.firstinspires.ftc.teamcode.cameraAutonomous;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.TouchSensor;
+import com.qualcomm.robotcore.util.Range;
 
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.teamcode.encoderExampleAutonomous.TurnLeft;
 import org.firstinspires.ftc.teamcode.stateMachine.StateBase;
 import org.firstinspires.ftc.teamcode.stateMachine.StateMachine;
 
@@ -48,5 +51,25 @@ public class State1 extends StateBase {
         addTelemetry("Blob Center", "%f, %f",
                 opmode.getCenterOfBiggestBlob().x,
                 opmode.getCenterOfBiggestBlob().y);
+
+        double leftPower;
+        double rightPower;
+
+        DcMotor leftDrive  = hardwareMap.get(DcMotor.class, "motor_1");
+        DcMotor rightDrive = hardwareMap.get(DcMotor.class, "motor_2");
+        double drive = .5;
+        double turn = 0.0;
+
+        if (opmode.getCenterOfBiggestBlob().x > 80) {
+            addTelemetry("Turn", "Right");
+
+        } else {
+            addTelemetry("Turn", "Left");
+
+        }
+        leftPower    = Range.clip(drive + turn, -1.0, 1.0) ;
+        rightPower   = Range.clip(drive - turn, -1.0, 1.0) ;
+        leftDrive.setPower(leftPower);
+        rightDrive.setPower(rightPower);
     }
 }
